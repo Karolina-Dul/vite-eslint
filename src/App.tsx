881e4eaf06e055f822e304add412e6dcb155cbe4
KarolinaDuljas <duljas.karolina@gmail.com>
2023-07-02 17:03:50 +0200

    Postawiona apka

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("count");
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    setText("licznik");
+  };
 
   return (
     <>
@@ -18,8 +24,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleClick}>
+          {text} is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,9 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
